8e2465988d92fcc044a6f0f12a4d948a978de500
MuratTuz <murattuzlali@gmail.com>
2021-04-24 13:33:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/category">Category</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/category" component={Category} />
+        </Switch>
+      </main>
     </div>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Home Componet </h1>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1> About Componet </h1>
+    </div>
+  );
+};
+
+const Category = () => {
+  return (
+    <div>
+      <h1> Category Componet </h1>
+    </div>
+  );
+};
 
 export default App;
